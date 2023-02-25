f71e743c5bff86849fad7ed78053665e4b093955
mojtaba <user@mysite.com>
2023-02-24 22:34:22 -0500

    add sources to board

--- frontend/src/components/Board/Board.jsx
+++ frontend/src/components/Board/Board.jsx
@@ -1,4 +1,8 @@
+import './Board.scss';
 import React from 'react';
+import classNames from 'classnames';
+import Tile from 'components/ColorBox/Tile';
+import Source from 'components/ColorBox/Source';
 import { number, func } from 'prop-types';
 
 const Board = ({
@@ -13,12 +17,25 @@ const Board = ({
 		});
 	};
 
-	const Rows = new Array(width);
+	const Sources = ({ position, direction }) => {
+		const ulClassName = classNames('sources', position, direction);
+		return (
+			<ul className={ulClassName}>
+				{[...Array(width)].map((_, ind) => <Source key={ind} />)}
+			</ul>
+		);
+	};
+
 	const Columns = new Array(height);
 
 	return (
 		<div className='Board'>
-
+			<div className='boardGrid'>
+				<Sources position='top' direction='row' />
+				<Sources position='left' direction='column' />
+				<Sources position='right' direction='column'/>
+				<Sources position='bottom' direction='row'/>
+			</div>
 		</div>
 	);
 };
